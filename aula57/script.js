96b5b6187716398fd5b9a454a25b39e8c84edc4c
douglasmcl <douglasmacielaraujo@outlook.com>
2023-12-06 21:45:52 -0300

    commit inicial

--- aula57/script.js
+++ aula57/script.js
@@ -4,12 +4,20 @@ function criarCalculadora() {
         inicia() {
             this.clicarBotao();
         },
+        clearDisplay() {
+            this.display.value = '';
+        },
+        backspaceDisplay() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
         clicarBotao() {
             document.addEventListener('click', (e) => { //se usar arrow function o this vai se manter sem precisar usar 'bind'
                 const el = e.target;
                 if(el.classList.contains('btn-num')) {
                     this.btnParaDisplay(el.innerText);
                 }
+                if(el.classList.contains('btn-clear')) this.clearDisplay();
+                if(el.classList.contains('btn-del')) this.backspaceDisplay();
             })
         },
         btnParaDisplay(number) {

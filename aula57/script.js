6bb58a683095df5e38ff5fea0e72c2fe369d7895
douglasmcl <douglasmacielaraujo@outlook.com>
2023-12-07 00:25:17 -0300

    mostra o resultado e quando pressionamos enter ele dispara o resultado também

--- aula57/script.js
+++ aula57/script.js
@@ -2,13 +2,40 @@ function criarCalculadora() {
     return {
         display: document.querySelector('.display'),
         inicia() {
+            this.selectDisplay();
+            this.pressionaEnter();
             this.clicarBotao();
+        },
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13) this.resultDisplay();
+            })
+        },
+        selectDisplay() {
+            this.display.select();
         },
         clearDisplay() {
             this.display.value = '';
         },
         backspaceDisplay() {
             this.display.value = this.display.value.slice(0, -1);
+        },
+        resultDisplay() {
+            let conta = this.display.value;
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta inválida');
+                    return;
+                }
+                
+                this.display.value = String(conta);
+            } catch(e) {
+                alert('Conta inválida');
+                conta = '';
+                return;
+            }
         },
         clicarBotao() {
             document.addEventListener('click', (e) => { //se usar arrow function o this vai se manter sem precisar usar 'bind'
@@ -18,6 +45,7 @@ function criarCalculadora() {
                 }
                 if(el.classList.contains('btn-clear')) this.clearDisplay();
                 if(el.classList.contains('btn-del')) this.backspaceDisplay();
+                if(el.classList.contains('btn-eq')) this.resultDisplay();
             })
         },
         btnParaDisplay(number) {

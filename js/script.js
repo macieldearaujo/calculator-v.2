523e14d6520bc9ed7cffdff278fd320386ad1c16
douglasmcl <douglasmacielaraujo@outlook.com>
2023-12-07 22:03:22 -0300

    refiz o código com função construtora

--- js/script.js
+++ js/script.js
@@ -1,58 +1,53 @@
-function criarCalculadora() {
-    return {
-        display: document.querySelector('.display'),
-        inicia() {
-            this.selectDisplay();
-            this.pressionaEnter();
-            this.clicarBotao();
-        },
-        pressionaEnter() {
-            this.display.addEventListener('keyup', e => {
-                if(e.keyCode === 13) this.resultDisplay();
-            })
-        },
-        selectDisplay() {
-            this.display.select();
-        },
-        clearDisplay() {
-            this.display.value = '';
-        },
-        backspaceDisplay() {
-            this.display.value = this.display.value.slice(0, -1);
-        },
-        resultDisplay() {
-            let conta = this.display.value;
-            try {
-                conta = eval(conta);
+function Calculadora() {
+    const display = document.querySelector('.display');
 
-                if(!conta) {
-                    alert('Conta inválida');
-                    return;
-                }
-                
-                this.display.value = String(conta);
-            } catch(e) {
+    this.gerarResultado = () => {
+        try {
+            const conta = eval(display.value);
+
+            if(!conta) {
                 alert('Conta inválida');
-                conta = '';
                 return;
             }
-        },
-        clicarBotao() {
-            document.addEventListener('click', (e) => { //se usar arrow function o this vai se manter sem precisar usar 'bind'
-                const el = e.target;
-                if(el.classList.contains('btn-num')) {
-                    this.btnParaDisplay(el.innerText);
-                }
-                if(el.classList.contains('btn-clear')) this.clearDisplay();
-                if(el.classList.contains('btn-del')) this.backspaceDisplay();
-                if(el.classList.contains('btn-eq')) this.resultDisplay();
-            })
-        },
-        btnParaDisplay(number) {
-            this.display.value += number;
+            display.value = conta;
+        } catch(e) {
+            alert('Conta inválida.');
+            return;
         }
+    };
+
+    this.display = num => {
+        display.value += num.innerText;
+        display.focus();
     }
+
+    this.clear = () => display.value = '';
+
+    this.del = () => display.value = display.value.slice(0, -1);
+
+    this.apertarBotao = () => {
+        display.addEventListener('keyup', (e) => {
+            const el = e.keyCode;
+            if(el == 13) this.gerarResultado();
+            if(el == 27) this.clear();
+        })
+    }
+
+    this.clicarBotao = () =>{
+        document.addEventListener('click', (e) => {
+            const el = e.target;
+
+            if(el.classList.contains('btn-eq')) this.gerarResultado();
+            if(el.classList.contains('btn-num')) this.display(el);
+            if(el.classList.contains('btn-clear')) this.clear();
+            if(el.classList.contains('btn-del')) this.del();
+        })
+    };
+    this.iniciar = () => {
+        this.clicarBotao();
+        this.apertarBotao();
+    };
 }
 
-const calculadora = criarCalculadora();
-console.log(calculadora.inicia())
+const calculadora = new Calculadora();
+calculadora.iniciar()
